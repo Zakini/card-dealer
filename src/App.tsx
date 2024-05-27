7f3bd964c6c62f198e3b408b3be762a8596bb780
Josh Wells <josh@parall.ax>
2024-05-27 19:29:47 +0100

    Moved deal animation into card component

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,17 @@ import Card from './components/Card'
 import { motion, AnimatePresence } from 'framer-motion'
 
 function App() {
-  // The card has started being dealt, but is still animating in
-  const [cardDealing, setCardDealing] = useState(false)
-  // The card has been fully dealt
   const [cardDealt, setCardDealt] = useState(false)
 
   return (
     // z-0 starts a new stacking context here (I think?)
-    <div className="relative z-0 bg-black" onClick={() => { setCardDealing(true) }}>
+    <div className="relative z-0 bg-black" onClick={() => { setCardDealt(true) }}>
       <div className="h-screen overflow-hidden z-10 flex items-center justify-center">
-        <AnimatePresence>
-          {(cardDealing || cardDealt) && (
-            // NOTE we use 100vh since the container fills the screen, and 50% since that's the
-            // height of the card. Update this if either of those changes!
-            <motion.div
-              initial={{ y: 'calc(100vh - 50%)' }}
-              animate={{ y: '0vh', transition: { duration: 2, ease: 'easeOut' } }}
-              onAnimationComplete={() => {
-                setCardDealing(false)
-                setCardDealt(true)
-              }}
-            >
-              <Card className="h-1/2" />
-            </motion.div>
-          )}
-        </AnimatePresence>
+        {cardDealt && <Card className="h-1/2" initialY="calc(100vh - 50%)" />}
       </div>
+
       <AnimatePresence>
-        {!cardDealing && !cardDealt && (
+        {!cardDealt && (
           <motion.div
             className="absolute inset-0 -z-10 flex items-center justify-center"
             exit={{ opacity: 0, transition: { duration: 1 } }}
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,10 @@
+import { motion, AnimatePresence, CustomValueType } from 'framer-motion'
 import cardBack from '../assets/cards/default/back.svg'
 import { useState } from 'react'
 
 interface Props {
   className?: string
+  initialY: string | number | CustomValueType
 }
 
 const deck = Object.values(import.meta.glob<string>(
@@ -10,20 +12,31 @@ const deck = Object.values(import.meta.glob<string>(
   { eager: true, import: 'default' },
 ))
 
-export default function Card({ className = '' }: Props) {
+export default function Card({ className = '', initialY }: Props) {
   // useState so that the chosen card doesn't change on render
   const [card] = useState(deck[Math.floor(Math.random() * deck.length)])
   const [faceUp, setFaceUp] = useState(false)
-  // TODO set this to true when card deal animation ends
-  const cardDealt = true
+  const [cardDealt, setCardDealt] = useState(false)
 
   return (
-    <img
-      className={`${className} aspect-auto`}
-      src={faceUp ? card : cardBack}
-      onClick={() => {
-        if (cardDealt) setFaceUp(true)
-      }}
-    />
+    <AnimatePresence>
+      {/*
+        NOTE we use 100vh since the container fills the screen, and 50% since that's the
+        height of the card. Update this if either of those changes!
+      */}
+      <motion.div
+        initial={{ y: initialY }}
+        animate={{ y: '0vh', transition: { duration: 2, ease: 'easeOut' } }}
+        onAnimationComplete={() => { setCardDealt(true) }}
+      >
+        <img
+          className={`${className} aspect-auto`}
+          src={faceUp ? card : cardBack}
+          onClick={() => {
+            if (cardDealt) setFaceUp(true)
+          }}
+        />
+      </motion.div>
+    </AnimatePresence>
   )
 }
