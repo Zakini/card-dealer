732aa71f351aed1c55d4a3087216048addf5b08c
Josh Wells <josh@parall.ax>
2024-05-27 21:51:28 +0100

    Added flip animation

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,9 +12,12 @@ const deck = Object.values(import.meta.glob<string>(
   { eager: true, import: 'default' },
 ))
 
-export default function Card({ className = '', initialY }: Props) {
+export default function Card({ className: inputClassName = '', initialY }: Props) {
+  const className = `${inputClassName} aspect-auto`
+
   // useState so that the chosen card doesn't change on render
   const [card] = useState(deck[Math.floor(Math.random() * deck.length)])
+  const [flipping, setFlipping] = useState(false)
   const [faceUp, setFaceUp] = useState(false)
   const [cardDealt, setCardDealt] = useState(false)
 
@@ -24,19 +27,44 @@ export default function Card({ className = '', initialY }: Props) {
         NOTE we use 100vh since the container fills the screen, and 50% since that's the
         height of the card. Update this if either of those changes!
       */}
-      <motion.div
-        initial={{ y: initialY }}
-        animate={{ y: '0vh', transition: { duration: 2, ease: 'easeOut' } }}
-        onAnimationComplete={() => { setCardDealt(true) }}
-      >
-        <img
-          className={`${className} aspect-auto`}
-          src={faceUp ? card : cardBack}
-          onClick={() => {
-            if (cardDealt) setFaceUp(true)
+      {!flipping && !faceUp && (
+        <motion.div
+          initial={{ y: initialY }}
+          variants={{
+            deal: { y: '0vh', transition: { duration: 2, ease: 'easeOut' } },
+            flip: { scaleX: 0, transition: { ease: 'easeOut' } },
           }}
-        />
-      </motion.div>
+          animate="deal"
+          exit="flip"
+          onAnimationComplete={(def) => {
+            if (def === 'deal') setCardDealt(true)
+            else if (def === 'flip') setFaceUp(true)
+          }}
+        >
+          <img
+            className={className}
+            src={cardBack}
+            onClick={() => {
+              if (cardDealt) setFlipping(true)
+            }}
+          />
+        </motion.div>
+      )}
+      {faceUp && (
+        <motion.div
+          initial={{ scaleX: 0 }}
+          animate={{ scaleX: 1, transition: { ease: 'easeIn' } }}
+          onAnimationComplete={() => { setCardDealt(true) }}
+        >
+          <img
+            className={className}
+            src={card}
+            onClick={() => {
+              if (cardDealt) setFaceUp(true)
+            }}
+          />
+        </motion.div>
+      )}
     </AnimatePresence>
   )
 }
