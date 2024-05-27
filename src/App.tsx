e3501959116715407b1e6728c075e2eae45878cd
Josh Wells <josh@parall.ax>
2024-05-27 18:01:40 +0100

    Cards can now be dealt

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Card from './components/Card'
 const possibleCardNames = Object.keys(cardMap)
 
 function App() {
+  const [cardDealt, setCardDealt] = useState(false)
   const [card, setCard] = useState<string | null>(null)
   const [faceUp, setFaceUp] = useState(false)
 
@@ -13,8 +14,23 @@ function App() {
   }, [])
 
   return (
-    <div className="h-screen flex items-center justify-center">
-      <Card cardName={card} faceUp={faceUp} className="h-1/2" onClick={() => { setFaceUp(s => !s) }} />
+    // z-0 starts a new stacking context here (I think?)
+    <div className="relative z-0 bg-black" onClick={() => { setCardDealt(true) }}>
+      <div className="h-screen z-10 flex items-center justify-center">
+        {cardDealt && (
+          <Card
+            cardName={card}
+            faceUp={faceUp}
+            className="h-1/2"
+            onClick={() => { setFaceUp(true) }}
+          />
+        )}
+      </div>
+      <div className="absolute inset-0 -z-10 flex items-center justify-center">
+        <span className="text-gray-900 text-9xl uppercase font-bold select-none">
+          Click to Deal
+        </span>
+      </div>
     </div>
   )
 }
